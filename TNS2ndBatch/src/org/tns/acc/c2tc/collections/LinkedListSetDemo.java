2902c8caf10dfb5049a5b96b566e69e1d7f4a0b3
ankit <ankit@192.168.8.109>
2026-02-27 23:20:16 +0530

    initial commit

---
+++ TNS2ndBatch/src/org/tns/acc/c2tc/collections/LinkedListSetDemo.java
@@ -0,0 +1,15 @@
+package org.tns.acc.c2tc.collections;
+
+import java.util.LinkedHashSet;
+
+public class LinkedListSetDemo {
+	public static void main(String[] args) {
+		LinkedHashSet<String> set=new LinkedHashSet<>();
+		set.add("cherry");
+		set.add("banana");
+		set.add("apple");
+		set.add(null);
+		set.add(null);
+		System.out.println("LinkedHashSet   " + set);
+	}
+}
