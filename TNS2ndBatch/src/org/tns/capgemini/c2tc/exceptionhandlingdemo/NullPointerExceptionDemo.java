d8802611cf24cc5d60009c01152325ddb5a6d7d2
ankit <ankit@192.168.8.109>
2026-02-27 21:54:25 +0530

    initial commit

---
+++ TNS2ndBatch/src/org/tns/capgemini/c2tc/exceptionhandlingdemo/NullPointerExceptionDemo.java
@@ -0,0 +1,24 @@
+package org.tns.capgemini.c2tc.exceptionhandlingdemo;
+
+public class NullPointerExceptionDemo {
+
+	
+
+	    public static void main(String[] args) {
+
+	        String str = null;
+
+	        try {
+	            if (str != null) {
+	                System.out.println(str.length());
+	            } else {
+	                System.out.println("String is null");
+	            }
+	        } catch (NullPointerException e) {
+	            System.out.println(e);
+	        } finally {
+	            System.out.println("No matter what this block we get executed");
+	        }
+	    }
+	}
+
