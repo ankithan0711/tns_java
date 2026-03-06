cec6e5baeaf8a72a1f14d9a540af3e5b2cda4399
ankit <ankit@192.168.8.109>
2026-03-07 00:00:00 +0530

    initial commit

---
+++ TNS2ndBatch/src/org/tnsif/acc/c2tc/interfacedemo/FunctionalInterfaceDemo.java
@@ -0,0 +1,27 @@
+package org.tnsif.acc.c2tc.interfacedemo;
+@FunctionalInterface
+interface InterfaceDemo2
+{
+    void sayHello(String Name);
+}
+class GreetingsImp implements InterfaceDemo2
+{
+
+	@Override
+	public void sayHello(String Name) {
+		System.out.println("Hello" + "   "+ Name + " !");
+		
+	}
+	
+}
+
+
+public class FunctionalInterfaceDemo {
+public static void main(String[] args) {
+		
+		InterfaceDemo2 obj=new GreetingsImp();
+		obj.sayHello("Vishu");
+		
+	}
+
+}
